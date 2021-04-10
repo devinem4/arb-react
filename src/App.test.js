907e2276ad31b005e0a7070598f73bdad01eff47
Matt Devine <mddevine@gmail.com>
2021-04-10 11:50:14 -0400

    :alembic: Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1 = screen.getByText(/Can we Arb?/i);
+  expect(h1).toBeInTheDocument();
+});
+
+test("BetInputs shows on load", () => {
+  render(<App />);
+  const inputFav = screen.queryByText("Favorite:");
+  expect(inputFav).toBeInTheDocument();
+});
+
+test("ArbOutputs doesnt show by default", () => {
+  render(<App />);
+  const happyFace = screen.queryByText(/🤑/i);
+  expect(happyFace).not.toBeInTheDocument();
+  const sadFace = screen.queryByText(/😭/i);
+  expect(sadFace).not.toBeInTheDocument();
 });
